7b0b3c18a7cbfe0e406b09aa85ccb66bfd4fed56
Tal <Tal@TAL-COMPUTER>
2019-02-12 15:33:57 +0200

    added player shooting

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -3,30 +3,35 @@ using System.Collections;
 
 public class EnemyMovement : MonoBehaviour
 {
-    Transform player;
-    //PlayerHealth playerHealth;
-    //EnemyHealth enemyHealth;
-    UnityEngine.AI.NavMeshAgent nav;
+    Transform player;                              // Reference to the player's position.
+    PlayerHealth playerHealth;                     // Reference to the player's health.
+    EnemyHealth enemyHealth;                       // Reference to this enemy's health.
+    UnityEngine.AI.NavMeshAgent nav;               // Reference to the nav mesh agent.
 
 
-    void Awake ()
+    void Awake()
     {
-        player = GameObject.FindWithTag ("Player").transform;
-        //playerHealth = player.GetComponent <PlayerHealth> ();
-        //enemyHealth = GetComponent <EnemyHealth> ();
-        nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+        // Set up the references.
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+        playerHealth = player.GetComponent<PlayerHealth>();
+        enemyHealth = GetComponent<EnemyHealth>();
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
     }
 
 
-    void Update ()
+    void Update()
     {
-        //if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
-        //{
-            nav.SetDestination (player.position);
-        //}
-        //else
-        //{
-            //nav.enabled = false;
-        //}
+        // If the enemy and the player have health left...
+        if (enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        {
+            // ... set the destination of the nav mesh agent to the player.
+            nav.SetDestination(player.position);
+        }
+        // Otherwise...
+        else
+        {
+            // ... disable the nav mesh agent.
+            nav.enabled = false;
+        }
     }
 }
