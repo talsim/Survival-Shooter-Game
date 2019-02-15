ffcb0f98ab9f7581149082f1066686263e76d0e1
Tal <Tal@TAL-COMPUTER>
2019-02-15 17:44:04 +0200

    added spawning enemies

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -5,7 +5,7 @@ using System.Collections;
 public class EnemyAttack : MonoBehaviour
 {
     public float timeBetweenAttacks = 0.5f;     // The time in seconds between each attack.
-    public int attackDamage = 10;               // The amount of health taken away per attack.
+    public int attackDamage = 20;               // The amount of health taken away per attack.
 
 
     Animator anim;                              // Reference to the animator component.
--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -2,10 +2,10 @@
 
 public class EnemyHealth : MonoBehaviour
 {
-    public int startingHealth = 100;            // The amount of health the enemy starts the game with.
+    public int startingHealth = 200;            // The amount of health the enemy starts the game with.
     public int currentHealth;                   // The current health the enemy has.
     public float sinkSpeed = 2.5f;              // The speed at which the enemy sinks through the floor when dead.
-    public int scoreValue = 10;                 // The amount added to the player's score when the enemy dies.
+    public int scoreValue = 30;                 // The amount added to the player's score when the enemy dies.
     public AudioClip deathClip;                 // The sound to play when the enemy dies.
 
 
